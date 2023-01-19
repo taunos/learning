a7fc6ff51c7714199ac0d0abe75d3ab4aef1aa1a
Bruno Dembogurski <brunodembogurski@gmail.com>
2023-01-19 18:17:34 -0300

    build: install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.{edge,js,ts,jsx,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
