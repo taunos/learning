8e4c52e799daebab951219f2354af512aab42d76
Bruno Dembogurski <brunodembogurski@gmail.com>
2023-02-16 17:15:55 -0300

    feature users pagination

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,22 +1,26 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-import User from 'App/Models/User'
-
 export default class AuthController {
   public async create({ view }: HttpContextContract) {
     return view.render('auth/create')
   }
 
-  public async store({ request, response, auth }: HttpContextContract) {
+  public async store({ request, response, auth, session }: HttpContextContract) {
     const { email, password } = request.only(['email', 'password'])
 
-    await auth.use('web').attempt(email, password)
+    try{
+      await auth.use('web').attempt(email, password)
+    }
+    catch(e){
+      session.flash({ errors: {login: 'blah'}})
+      return response.redirect().toRoute('auth.create')
+    }
 
     return response.redirect().toRoute('admin.index')
   }
 
   public async destroy({ auth, response }: HttpContextContract) {
     await auth.use('web').logout()
-    response.redirect().toRoute('/')
+    response.redirect().toRoute('admin.index')
   }
 }
